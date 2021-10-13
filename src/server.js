f3825e0dfd633e5fa691c0de23b27dc3a328902f
airlynoise <90142987+airlynoise@users.noreply.github.com>
2021-10-13 21:53:10 +0900

    wetube-clone-day4

--- src/server.js
+++ src/server.js
@@ -1,26 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express(); // express app 생성
 const logger = morgan("dev");
-
-const globalRouter = express.Router();
-const userRouter = express.Router();
-const videoRouter = express.Router();
-
-const home = (req, res) => {
-    return res.send("hello");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
